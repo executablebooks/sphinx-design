10d21dca25d6e30cd592884c8583cf0e7907f5d5
Chris Sewell <chrisj_sewell@hotmail.com>
2021-08-04 15:02:56 +0200

    📚 DOCS: Add logo

--- docs/conf.py
+++ docs/conf.py
@@ -10,6 +10,9 @@ extensions = ["myst_parser", "sphinx_design"]
 html_theme = os.environ.get("SPHINX_THEME", "alabaster")
 html_title = f"Sphinx Design ({html_theme.replace('_', '-')})"
 
+html_logo = "_static/logo_wide.svg"
+html_favicon = "_static/logo_square.svg"
+
 if html_theme == "alabaster":
     html_css_files = [
         "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/fontawesome.min.css"
