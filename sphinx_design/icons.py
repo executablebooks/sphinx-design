917506aecca88158872653513163439b6983d19f
12rambau <pierrick.rambaud49@gmail.com>
2023-02-04 11:08:58 +0100

    support any version of fa descriptor

--- sphinx_design/icons.py
+++ sphinx_design/icons.py
@@ -24,11 +24,25 @@ OCTICON_CSS = """\
   fill: currentColor;
 }"""
 
+FA_STYLES = {
+    "fa": "fa-solid",
+    "fas": "fa-solid",
+    "fab": "fa-brands",
+    "far": "fa-regular",
+    "fa-solid": "fa-solid",
+    "fa-brands": "fa-brands",
+    "fa-regular": "fa-regular"
+}
+"""The fontawesome correspondance table to make sure older fa descriptor does not raise errors
+fa is deprecated since v5 
+fas, fab and far are deprecated since v6
+"""
+
 
 def setup_icons(app: Sphinx) -> None:
     app.add_role("octicon", OcticonRole())
     app.add_directive("_all-octicon", AllOcticons)
-    for style in ["fas", "fab", "far", "fa-solid", "fa-brands", "fa-regular"]:
+    for style in FA_STYLES:
         # note: fas, fab, far are deprecated in v6,
         # fa-solid is the default and fa-brands is the other free option
         app.add_role(style, FontawesomeRole(style))
@@ -185,7 +199,7 @@ class FontawesomeRole(SphinxRole):
 
     def __init__(self, style: str) -> None:
         super().__init__()
-        self.style = style
+        self.style = FA_STYLES[style]
 
     def run(self) -> Tuple[List[nodes.Node], List[nodes.system_message]]:
         """Run the role."""
