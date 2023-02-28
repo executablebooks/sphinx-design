a67da1ebf8cf684868fa7d2b14f740eebc4cd216
Chris Sewell <chrisj_sewell@hotmail.com>
2023-02-28 14:55:47 +0100

    🚀 RELEASE: v0.4.0

--- sphinx_design/__init__.py
+++ sphinx_design/__init__.py
@@ -1,7 +1,7 @@
 """A sphinx extension for designing beautiful, view size responsive web components."""
 from typing import TYPE_CHECKING
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 if TYPE_CHECKING:
     from sphinx.application import Sphinx
