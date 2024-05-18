a699d5c489c391a3beceb45490bae687798ed095
Chris Sewell <chrisj_sewell@hotmail.com>
2024-05-18 20:27:29 +0200

    🧪 add regression test for available octicons

---
+++ tests/test_misc.py
@@ -0,0 +1,15 @@
+from sphinx_design.icons import get_octicon_data
+
+
+def test_octicons(file_regression):
+    """Test the available octicon names.
+    
+    This is intended to provide a diff of the octicons available,
+    when the octicons are updated, to check if we are removing any
+    (and hence breaking back-compatibility).
+    """
+    data = get_octicon_data()
+    content = ""
+    for octicon in sorted(get_octicon_data()):
+        content += f"{octicon}: {','.join(data[octicon]['heights'])}\n"
+    file_regression.check(content)
