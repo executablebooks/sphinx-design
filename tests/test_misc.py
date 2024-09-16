de8a3677161a2e01a26c1cf8e978547735ebb495
Chris Sewell <chrisj_sewell@hotmail.com>
2024-09-16 23:37:03 +0200

    🧪 add regression test for available material icons

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,4 +1,5 @@
-from sphinx_design.icons import get_octicon_data
+import pytest
+from sphinx_design.icons import get_octicon_data, get_material_icon_data
 
 
 def test_octicons(file_regression):
@@ -10,6 +11,21 @@ def test_octicons(file_regression):
     """
     data = get_octicon_data()
     content = ""
-    for octicon in sorted(get_octicon_data()):
+    for octicon in sorted(data):
         content += f"{octicon}: {','.join(data[octicon]['heights'])}\n"
+    file_regression.check(content)
+
+
+@pytest.mark.parametrize("style", ["regular", "outlined", "round", "sharp", "twotone"])
+def test_material(style, file_regression):
+    """Test the available material icons names.
+
+    This is intended to provide a diff of the octicons available,
+    when the octicons are updated, to check if we are removing any
+    (and hence breaking back-compatibility).
+    """
+    data = get_material_icon_data(style)
+    content = ""
+    for name in sorted(data):
+        content += f"{name}: {','.join(data[name]['heights'])}\n"
     file_regression.check(content)
